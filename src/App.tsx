077ec8802dcc5e85d745cd76f33d85c28fddf9e5
Anna Egorova <a.egorova@brightlab.me>
2023-06-23 17:50:40 +0300

    fix vite config

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,18 @@ export const App = () => {
       <Header/>
       <div className=" mx-auto relative overflow-hidden ">
         <img
-          src={'./src/images/png.webp'}
+          src={'png.webp'}
           style={{  transform: `translateX(${translateXPosition}) scaleX(120%)`, transitionTimingFunction: 'ease-in-out', transitionDuration:'480ms' }}
           className="  pointer-events-none opacity-20 object-cover transition z-10 top-0 right-0  bottom-0  left-0 absolute w-full h-full"
         />      
           <img
-          src={'./src/images/png1.png'}
+          src={'png1.png'}
           style={{  transform: `translateX(${translateXPosition}) scaleX(120%)`, transitionTimingFunction: 'ease-in-out', transitionDuration:'400ms' }}
           className="pointer-events-none opacity-40  object-cover transition z-10 top-0 -mr-100 -right-250 bottom-0 left-0 absolute w-full h-full"
         />
         <ImageSlider
-          leftImage="./src/images/freelance.webp"
-          rightImage="./src/images/office.webp"
+          leftImage="freelance.webp"
+          rightImage="office.webp"
           leftImageLabel={"FREELANCE"}
           rightImageLabel={"OFFICE"}
           rightImageLabelCss={"tracking-[18px] text-46 mobile:tracking-tight mobile:text-40"}

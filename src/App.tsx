6ec1f59faaa61760f1e9fc4bd833852d19efbe36
Anna Egorova <a.egorova@brightlab.me>
2023-07-17 10:58:38 +0300

    update nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,11 @@ export const App = () => {
       <div className=" mx-auto relative overflow-hidden ">
         <img
           src={'geometry.webp'}
-          className="animate-geometry-1 pointer-events-none object-cover transition z-10 top-0 right-0  bottom-0  left-0 absolute w-full h-full"
+          className="animate-geometry-1 pointer-events-none object-cover mobile:object-scale-down transition z-10 top-0 right-0  bottom-0  left-0 absolute w-full h-full"
         />      
           <img
           src={'geometry2.webp'}
-          className="animate-geometry-2 pointer-events-none object-cover transition z-10 top-0  bottom-0 left-0 right-0 absolute w-full h-full"
+          className="animate-geometry-2 pointer-events-none object-cover transition z-10 top-0 mobile:object-scale-down bottom-0 left-0 right-0 absolute w-full h-full"
         />
         <ImageSlider
           leftImage="freelance.webp"

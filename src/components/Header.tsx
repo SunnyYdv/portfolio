25aa1312ccdface25dc6495a42105bb7ef8d3ef3
Anna Egorova <a.egorova@brightlab.me>
2023-07-21 12:09:33 +0300

    add progress scroll barfix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,68 +23,85 @@ export const Header = () => {
     navigate("/home");
   }, []);
 
-
   useEffect(() => {
+    if (location.pathname.includes("home")) return;
+
     function scrollProgressBar() {
-      const winScroll = document.documentElement.scrollTop;
+      const scrollTop = document.documentElement.scrollTop;
+
       const height =
         document.documentElement.scrollHeight -
         document.documentElement.clientHeight;
-      const scrolled = (winScroll / height) * 100;
-      progressRef!.current!.style.width = scrolled + "%";
+
+      const scrolled = (scrollTop / height) * 100;
+
+      progressRef.current && (progressRef.current.style.width = scrolled + "%");
     }
 
-    window.addEventListener('scroll', ()=> scrollProgressBar())
+    window.addEventListener("scroll", () => scrollProgressBar());
 
-    return window.removeEventListener('scroll', ()=>scrollProgressBar())
-  });
+    return window.removeEventListener("scroll", () => scrollProgressBar());
+  }, [location]);
 
   return (
-    <nav
+    <div
       className={cls(
-        "fixed z-20 text-24 mobile:text-20 flex  items-center gap-x-20 p-20 px-40 mobile:px-20 justify-between w-full ",
+        "fixed z-20 text-24 mobile:text-20  max-w-screen  flex flex-col overflow-hidden w-full ",
         {
           "text-black bg-page": !location.pathname.includes("home"),
           "text-white bg-none ": location.pathname.includes("home"),
         }
       )}
     >
-      <img onClick={toHome} src="fhLogo.svg" className="cursor-pointer" />
+      <nav
+        className={cls(
+          " flex  items-center gap-x-20 p-20 px-40 mobile:px-20 justify-between w-full ",
+          {
+            "text-black bg-page": !location.pathname.includes("home"),
+            "text-white bg-none ": location.pathname.includes("home"),
+          }
+        )}
+      >
+        <img onClick={toHome} src="fhLogo.svg" className="cursor-pointer" />
+
+        <div className="space-x-40 mobile:space-x-20 mobile:text-16 font-thin">
+          <span
+            onClick={toBlog}
+            className={cls("cursor-pointer", {
+              "text-orange border-b pb-10 border-b-orange border-b-2 ":
+                location.pathname.includes("blog"),
+            })}
+          >
+            How it works
+          </span>
+          <span
+            onClick={toPlatforms}
+            className={cls("cursor-pointer", {
+              "text-orange border-b pb-10 border-b-orange border-b-2 ":
+                location.pathname.includes("platforms"),
+            })}
+          >
+            Platforms
+          </span>
+          <span
+            onClick={toAbouts}
+            className={cls("cursor-pointer", {
+              "text-orange border-b pb-10 border-b-orange border-b-2 ":
+                location.pathname.includes("about"),
+            })}
+          >
+            About
+          </span>
+        </div>
+      </nav>
       {!location.pathname.includes("home") && (
-        <div
-          ref={progressRef}
-          className=" ml-1rem  h-6 bg-orange rounded-30 absolute bottom-0 left-20"
-        />
+        <div className="px-20">
+          <div
+            ref={progressRef}
+            className="h-6 bg-orange rounded-30 "
+          />
+        </div>
       )}
-      <div className="space-x-40 mobile:space-x-20 mobile:text-16 font-thin">
-        <span
-          onClick={toBlog}
-          className={cls("cursor-pointer", {
-            "text-orange border-b pb-10 border-b-orange border-b-2 ":
-              location.pathname.includes("blog"),
-          })}
-        >
-          How it works
-        </span>
-        <span
-          onClick={toPlatforms}
-          className={cls("cursor-pointer", {
-            "text-orange border-b pb-10 border-b-orange border-b-2 ":
-              location.pathname.includes("platforms"),
-          })}
-        >
-          Platforms
-        </span>
-        <span
-          onClick={toAbouts}
-          className={cls("cursor-pointer", {
-            "text-orange border-b pb-10 border-b-orange border-b-2 ":
-              location.pathname.includes("about"),
-          })}
-        >
-          About
-        </span>
-      </div>
-    </nav>
+    </div>
   );
 };
